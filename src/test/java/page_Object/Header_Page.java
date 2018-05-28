9fc92b462dcfca8728d6ee066f727fb87072b699
kamolsavar <kamolsavar@gmail.com>
2018-05-28 22:20:13 +0100

    learning git

---
+++ src/test/java/page_Object/Header_Page.java
@@ -0,0 +1,7 @@
+package page_Object;
+
+import drivers.Driver_Factory;
+
+public class Header_Page extends Driver_Factory{
+//    driver.findElement()
+}
---
+++ src/test/java/page_Object/Result_Page.java
@@ -0,0 +1,4 @@
+package page_Object;
+
+public class Result_Page {
+}
